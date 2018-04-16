2222db566c5b1a939d49e6094a890d5089e06b19
ali <aliasgary73@gmail.com>
2018-04-17 00:41:21 +0430

    header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
-  <div id="app">salam</div>
+<div :class="$style.root">
+  <Header :year="year" :month="month" />
+</div>
 </template>
 
 <script>
+// components
+import Header from './components/Header'
+
+
 export default {
   name: 'App',
+
+  components: {
+    Header,
+  },
+
+  data: () => ({
+    year: '2018',
+    month: 'August',
+  })
 }
 </script>
 
 <style module>
 .root {
-  width: 300px;
-  height: 350px;
-  background: #EEEEEE;
-  border-radius: 10px;
+  background-color: #29323f;
+  width: 280px;
+  padding: 35px;
+  font-family: 'Dosis', sans-serif;
+  height: 330px;
+  border-radius: 5px;
   overflow: hidden;
+  box-shadow: 5px 10px 20px rgba(0, 0, 0, 0.6);
+  font-weight: bold;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  -webkit-transform: translateY(-50%) translateX(-50%);
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+<h1 :class="$style.root">
+  {{ month }} {{ year }}
+</h1>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+  props: ['year', 'month'],
+}
+</script>
+
+<style module>
+.root {
+  font-size: 24px;
+  font-weight: bolder;
+  text-align: center;
+  color: #fcee6d;
+  margin: 0 0 30px 0;
+}
+</style>
